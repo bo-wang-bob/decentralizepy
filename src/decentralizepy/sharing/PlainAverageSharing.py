b9b94a6d0c3f9da2ccbda0ea52c48ac09a2ffb6f
tktk3927 <15200459332>
2024-12-17 17:12:07 +0800

    fin

--- src/decentralizepy/sharing/PlainAverageSharing.py
+++ src/decentralizepy/sharing/PlainAverageSharing.py
@@ -104,7 +104,7 @@ class PlainAverageSharing(Sharing):
         """
         if(len(model_history)<=1):
             return -1,-1
-        T = 100
+        T = 30
         tao = 100000
         TAO_0 = 3e-7
         ALPHA = 0.999
@@ -144,22 +144,73 @@ class PlainAverageSharing(Sharing):
                     radius = aradius
             tao *= ALPHA
         return center,radius
-            
+    
+    def calculate_similarity(self,center,centerx,model_history,iteration):
+        T = 30
+        GAMA = 0.9
+
+        g_i=np.zeros_like(model_history[1])
+        for i in range(max(1,iteration-T+1),iteration):
+            g_i += np.exp(-GAMA*(iteration-1-i))*(model_history[i+1]-model_history[i])
+        
+        O_ik = centerx - center
+        if np.dot(g_i,O_ik) > 0:
+            Sim = 1
+        elif np.dot(g_i,O_ik) == 0:
+            Sim = 0
+        else:
+            Sim = -1
+        
+        Sim *= 1/(1+O_ik.norm())
+        return Sim
+    
+    def rep_evaluation(self,Sim_x,radius,radiusx,max_radius,min_radius):
+        B = 0.5
+        C = 0.5
+
+        pi = (Sim_x + 1)/2
+        ni = (1 - Sim_x)/2
+        ux = 1/(1+np.exp(-(radius+radiusx-2*min_radius)/(max_radius-min_radius)))
+        bx = (1-ux)*(B*pi/(B*pi+C*ni))
+        dx = (1-ux)*(C*ni/(B*pi+C*ni))
+        return bx,dx,ux
+    
     def _averaging(self, peer_deques,global_lr,model_history,iteration,my_neighbors):
         """
         Averages the received model with the local model
 
         """
+        A = 0.5 
+        centers,radiuss = dict(),dict()
         for x in model_history:
             model_history[x][iteration] = parameters_to_vector(self.deserialized_model(model_history[x][iteration])) 
         with torch.no_grad():
+            center,radius=self.superball_calculate(self,model_history[self.machine_id],iteration)
+            centers[self.machine_id] = center
+            radiuss[self.machine_id] = radius
             for x in my_neighbors:
-                center,radius=self.superball_calculate(self,model_history[x],iteration)
-            
+                centerx,radiusx=self.superball_calculate(self,model_history[x],iteration)
+                centers[x] = centerx
+                radiuss[x] = radiusx
+
+            reps = dict()
+            for x in my_neighbors:
+                Sim_x = self.calculate_similarity(self,centers[self.machine_id],centers[x],model_history[self.machine_id],iteration)
+                bx,dx,ux=self.rep_evaluation(self,Sim_x,radiuss[self.machine_id],radiuss[x],max(radiuss.values()),min(radiuss.values()))
+                repx=bx+A*ux
+                reps[x] = repx
+
+        ITA = 0.8
         self.received_this_round = 0
         with torch.no_grad():
+            Agg = dict()
+            Agg[self.machine_id] = 1
+            for x in reps:
+                if reps[x] > ITA:
+                    Agg[x] = reps[x]
+            
             total = dict()
-            weight = 1 / (len(peer_deques) + 1)
+            sums = sum(Agg.values())
             train_data = dict()
             for i, n in enumerate(peer_deques):
                 self.received_this_round += 1
@@ -172,30 +223,18 @@ class PlainAverageSharing(Sharing):
                         n, iteration
                     )
                 )
-                data = self.deserialized_model(data)
-                for name, param in data.items():
-                    if name not in train_data:
-                        train_data[name] = param.data.view(1, -1)
-                    else:
-                        train_data[name] = torch.cat((train_data[name], param.data.view(1, -1)),
-                                                        dim=0)
-                
-            model_updates = dict()
-            for (name, param), local_param in zip(self.model.state_dict().items(), train_data.values()):
-                model_updates[name] = local_param.data - param.data.view(1, -1)
-            
-            global_update = self.foolsgold(model_updates)
-            
-            for name, param in self.model.state_dict().items():
-                total[name] = param.data + global_lr * global_update[name].view(param.data.shape)
-            #     for key, value in data.items():
-            #         if key in total:
-            #             total[key] += value * weight
-            #         else:
-            #             total[key] = value * weight
+                if n in Agg:
+                    data = self.deserialized_model(data)
+                    for key, value in data.items():
+                        if key in total:
+                            total[key] += value * Agg[n]/sums
+                        else:
+                            total[key] = value * Agg[n]/sums
+                else:
+                    logging.info("Neighbor {} is not in Agg".format(n))
 
-            # for key, value in self.model.state_dict().items():
-            #     total[key] += value * weight
+            for key, value in self.model.state_dict().items():
+                total[key] += value * Agg[self.machine_id]/sums
 
         self.model.load_state_dict(total)
         self._post_step()
